19d3fb6bca78201d1c4bda4b8bf6f3f446108161
zainabnoori <zainab.noori@and.digital>
2023-02-03 15:10:21 +0000

    Setup contentful

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,22 @@ namespace App\Http\Controllers;
 
 use Inertia\Inertia;
 use Illuminate\Http\Request;
+use Contentful\Delivery\Client;
 
 class HomeController extends Controller
 {
+    private $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
     public function index()
     {
+        $entries = $this->client->getContents('page');
+        dd($entries);
+
+
         return Inertia::render('index');
     }
 
