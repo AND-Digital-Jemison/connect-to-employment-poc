12104cafb9e1aa1c6742f6c4e5ef9c37653bcd09
Natasha Mann <natasha.mann@and.digital>
2023-02-06 14:46:07 +0000

    graphql query build

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,27 +21,55 @@ class HomeController extends Controller
         $accessToken = env('CONTENTFUL_DELIVERY_TOKEN');
         $environment = env('CONTENTFUL_ENVIRONMENT_ID');
 
-        $endpoint = "https://graphql.contentful.com/content/v1/spaces/".$spaceID."/environments/".$environment;
+        $endpoint = "https://graphql.contentful.com/content/v1/spaces/" . $spaceID . "/environments/" . $environment;
 
         $query = <<<GQL
         query {
-            pageSection(id: "pageSection") {
-                title
+            pageSectionCollection(where: {title: "Articles"}, limit: 1) {
+                items {
+                    header {
+                       title
+                       pageTitle
+                       hero {
+                        title
+                        asset {
+                           title
+                           url
+                        }
+                       }
+                    }
+                    footer {
+                        text
+                    }
+                    body {
+                        ... on Articles {
+                            title
+                            articleListItemsCollection(limit:20) {
+                                items {
+                                    title
+                                    image {
+                                        asset {
+                                            url
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
         GQL;
 
         $response = Http::withHeaders([
             'Content-Type' => 'application/json',
-            'Authorization' => sprintf("Bearer %s",$accessToken)
+            'Authorization' => sprintf("Bearer %s", $accessToken)
         ])->post($endpoint, [
             'query' => $query
         ]);
 
 
         dump($response->json());
-
-
     }
 
     public function show()
