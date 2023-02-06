01d7ad158409d35ebb993965791bf173f18946cd
David Watson <davidwatson@CQ2R32N6R9.local>
2023-02-06 09:12:46 +0000

    page structure set up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,12 +18,20 @@ class HomeController extends Controller
     {
 
         $query = new \Contentful\Delivery\Query();
-        $query->setContentType('page');
+        $query->setContentType('pageSection')->where('fields.' . "title", "Articles")->setInclude(6);
+
 
         $entries = $this->client->getEntries($query);
+        $header = $entries[0]->get('header');
+        $body = $entries[0]->get('body');
+        $footer = $entries[0]->get('footer');
 
-        return Inertia::render('index',[
-            'entries' => $entries
+
+        return Inertia::render('index', [
+            'header' => ['pageTitle' => $header->pageTitle, 'hero' => $header->get('hero')],
+            'body' => ['introText' => $body->introText, 'articles' => $body->get('articleListItems')],
+            'footer' => ['footerText' => $footer->text]
+
         ]);
     }
 
--- resources/js/Pages/index.vue
+++ resources/js/Pages/index.vue
@@ -1,15 +1,33 @@
 <template>
-    <div>1</div>
+     <h1>Header:{{ props.header.pageTitle }}</h1>
+    <p>{{ props.body.introText }}</p>
+
+    <div>
+        <h2>Articles</h2>
+    <div v-for="article in props.body.articles">
+        <h3>{{ article.fields.title }}</h3>
+        <p>{{ article.fields.intro}}</p>
+    </div>
+    
+    </div>
+   
+
+    <div>
+        <h3>Footer</h3>
+        <p>{{ props.footer.footerText }}</p>
+        </div>
 </template>
 
 <script setup>
 import { onMounted } from "vue";
 const props = defineProps({
-    entries: Object,
+    header: Object,
+    body: Object,
+    footer: Object,
 });
 
 onMounted(() => {
-    console.log(props.entries.items[0].fields.animals);
+    console.log(props);
 });
 </script>
 
